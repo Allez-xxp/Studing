e9fb6003b8840e3633a81db1a6d663f0d4839091
Allez-xxp <2811632058@qq.com>
2020-04-14 17:24:53 +0800

    vue-router系列3 vue-router的两种传参方法整理

--- vueclitest/src/App.vue
+++ vueclitest/src/App.vue
@@ -1,16 +1,17 @@
 <template>
   <div id="app">
     <img src="./assets/logo.png">
-    <div>
+     <div>
       <!-- 导航做法：router-link标签 -->
        <router-link to="/">首页</router-link> | <!--相当于a标签，链接作用 -->
        <router-link to="/Hi">Hi页</router-link> |
        <router-link to="/Hi/H1">H1页</router-link> |
        <router-link :to="{name:'H2', params:{username:'allez', id:'1712'}}">H2页</router-link> 
     </div>
-    <p>{{$route.name}}</p>
-    <!-- 全局路由 -->
-    <router-view/>  
+   <p>{{$route.name}}</p>
+   <!--  全局路由 -->
+    <router-view></router-view>
+    
     
   </div>
 </template>
--- vueclitest/src/router/index.js
+++ vueclitest/src/router/index.js
@@ -11,7 +11,8 @@ export default new Router({
     {                    //每一个链接都是一个对象
       path: '/',         //链接路径
       name: 'HelloWorld',     //路由名称，
-      component: HelloWorld   //对应的组件模板
+      component: HelloWorld      //对应的组件模板
+           
     },
     {
       path: '/Hi',
