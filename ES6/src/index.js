47963fdb9e972e5deedacfcf37eab0be8be4cc95
Allez-xxp <2811632058@qq.com>
2020-05-25 22:21:19 +0800

    ES6系列基础知识学习3 数组解构赋值4点，字符串解构

--- ES6/src/index.js
+++ ES6/src/index.js
@@ -19,3 +19,39 @@
 // var a = "allez";
 // // 报错：常量声明不可以再赋值
 // console.log(a);
+
+// 数组解构赋值
+// 数组模式和赋值模式统一；若不统一，获得undefined或者直接报错。
+// 1. 直接赋值
+// let [a,b,c]=[1,2,3];
+// console.log(a+b+c);
+
+// 2.解构赋值是允许你使用默认值的
+// let [foo = true] =[];
+// console.log(foo); //控制台打印出true
+// let [a,b="JSPang"]=['技术胖']
+// console.log(a+b); //控制台显示“技术胖JSPang”
+
+// 3.undefined和null的区别：undefined相当于什么都没有；null相当于有值，但值为null。
+// let [a,b="JSPang"]=['技术胖',undefined];
+// console.log(a+b); //控制台显示“技术胖JSPang”
+// let [a,b="JSPang"]=['技术胖',null];
+// console.log(a+b); //控制台显示“技术胖null”
+
+// 4.解构不仅可以用于数组，还可以用于对象。
+// 注意：对象的解构与数组有一个重要的不同。
+// 数组的元素是按次序排列的，变量的取值由它的位置决定；
+// 而对象的属性没有次序，变量必须与属性同名，才能取到正确的值。
+// let {f,b} = {f:'xxp',b:'allez'};
+// console.log(f+b);
+
+// 若提前定义了变量，解构时需要在最外加圆括号()
+// let foo;
+// ({foo} = {foo:'xxp'});
+// console.log(foo);
+
+// 字符串解构（此时字符串被转换成了一个类似数组的对象。）
+const [a,b,c] = 'xxp';
+console.log(a);
+console.log(b);
+console.log(c);
