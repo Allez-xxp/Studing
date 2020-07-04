b5f6e85367b8ee6faa38c03846f4e470a8cd80a7
Allez-xxp <2811632058@qq.com>
2020-07-04 21:30:39 +0800

    ES6系列基础知识学习16 promise对象（解决es5的回调地狱问题）

--- es6demo/src/index.js
+++ es6demo/src/index.js
@@ -436,16 +436,57 @@
     
 // }) 
 // (3) apply的作用是调用内部的方法，它使用在方法体是一个匿名函数时
-    let target = function () {
-        return 'I am JSPang';
-    };
-    let handler = {
-        apply(target, ctx, args) {
-            console.log('do apply');
-            return Reflect.apply(...arguments);
-        }
-    }
+    // let target = function () {
+    //     return 'I am JSPang';
+    // };
+    // let handler = {
+    //     apply(target, ctx, args) {
+    //         console.log('do apply');
+    //         return Reflect.apply(...arguments);
+    //     }
+    // }
     
-    let pro = new Proxy(target, handler);
+    // let pro = new Proxy(target, handler);
     
-    console.log(pro());
+    // console.log(pro());
+
+// 16. promise对象：解决es5的回调地狱问题
+// let state=1;
+
+// function step1(resolve,reject){
+//     console.log('1.开始-洗菜做饭');
+//     if(state==1){
+//         resolve('洗菜做饭--完成');
+//     }else{
+//         reject('洗菜做饭--出错');
+//     }
+// }
+
+// function step2(resolve,reject){
+//     console.log('2.开始-坐下来吃饭');
+//     if(state==1){
+//         resolve('坐下来吃饭--完成');
+//     }else{
+//         reject('坐下来吃饭--出错');
+//     }
+// }
+
+// function step3(resolve,reject){
+//     console.log('3.开始-收拾桌子洗完');
+//      if(state==1){
+//         resolve('收拾桌子洗完--完成');
+//     }else{
+//         reject('收拾桌子洗完--出错');
+//     }
+// }
+// // 先定义每个步骤的方法，再声明promise，按照顺序调用方法
+// new Promise(step1).then(function(val){
+//     console.log(val);
+//     return new Promise(step2);
+// }).then(function(val){
+//      console.log(val);
+//     return new Promise(step3);
+// }).then(function(val){
+//     console.log(val);
+//     return val;
+// });
